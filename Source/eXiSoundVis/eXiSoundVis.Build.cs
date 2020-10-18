bc1ea3c4088f1626dbf29d4db200aba8903535c3
James Lennon <jl@amstaddigital.com>
2020-10-18 03:19:10 -0700

    Initial commit of original plugin

---
+++ Source/eXiSoundVis/eXiSoundVis.Build.cs
@@ -0,0 +1,23 @@
+using UnrealBuildTool;
+using System.IO;
+
+public class eXiSoundVis : ModuleRules
+{
+	public eXiSoundVis(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        DefaultBuildSettings = BuildSettingsVersion.V2;
+
+        PublicIncludePaths.AddRange(new string[] {
+            //"eXiSoundVis/Public"
+        });
+
+        PrivateIncludePaths.AddRange(new string[] {
+            //"eXiSoundVis/Private"
+        });
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Engine", "Core", "CoreUObject", "InputCore", "RHI", "Kiss_FFT" });
+
+        PublicDependencyModuleNames.Add("Kiss_FFT");
+	}
+}
